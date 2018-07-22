25438216e82f1393bbae803736943bf0367d023d
mohit <anand.mohit001@gmail.com>
2018-07-22 11:07:21 +0530

    object_oriented_beginning

---
+++ object_or/member_initializer.cpp
@@ -0,0 +1,68 @@
+// https://www.youtube.com/watch?v=1nfuYMXjZsA&index=36&list=PLlrATfBNZ98dudnM48yfGUldqGD0S4FFb
+// Comment/Uncomment sections of code to see the output difference
+
+
+#include<iostream>
+
+class Example{
+
+public:
+	Example()
+	{
+		std::cout<<"hello 1" << std::endl;
+	}
+
+	Example(int x)
+	{
+		std::cout<<"hello 2" << " " << x << std::endl;
+	}
+
+
+};
+
+
+class Entity{
+
+private:
+	int a;
+	std::string name;
+	Example x;
+
+public:
+	// Constructor
+	// Entity()
+	// {
+	// 	this->a = 1;
+	// 	this->name = "mohit";
+	// 	this->x = 8;
+	// }
+
+	// Constructor using member initializer list
+	Entity()
+	: a(5), name("mohit"), x(8)
+	{ 	}
+
+
+	// Parameterized Constructor
+	// Entity(int a , const string &name )
+	// {
+	// 	this->a = a;
+	// 	this->name = name;
+	// }
+
+	// Member initializer list corresponding to Parameterized Constructor
+
+	Entity(int x, const std::string &nam)
+	: a(x), name(nam)
+	{	}
+
+};
+
+
+int main()
+{	
+
+	Entity r;
+
+	return 0;
+}
